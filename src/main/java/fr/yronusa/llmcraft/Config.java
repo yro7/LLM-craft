37001db360b16eb7dccfdd57863083b31e5d55a2
Yronusa <yronusa2000@gmail.com>
2024-06-05 22:50:57 +0200

    Now using LC4J as the main way to use LLm's APIs.

--- src/main/java/fr/yronusa/llmcraft/Config.java
+++ src/main/java/fr/yronusa/llmcraft/Config.java
@@ -7,6 +7,8 @@ public class Config {
     public static FileConfiguration config;
     public static String openAI;
 
+
+
     public static void load() {
         config = LLM_craft.getInstance().getConfig();
         openAI = config.getString("openAI");
--- src/main/java/fr/yronusa/llmcraft/Model.java
+++ src/main/java/fr/yronusa/llmcraft/Model.java
@@ -1,4 +1,8 @@
 package fr.yronusa.llmcraft;
 
 public interface Model {
+
+    public static enum MODEL {
+        OPENAI,
+    }
 }
