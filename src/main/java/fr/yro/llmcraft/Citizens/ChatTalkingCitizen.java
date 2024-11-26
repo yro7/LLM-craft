92152394bb6d99c4cec74ab43ce15987702d1914
Yronusa <yronusa2000@gmail.com>
2024-11-26 12:32:11 +0100

    Delegated most of class' logic to TalkingCitizen

--- src/main/java/fr/yro/llmcraft/Citizens/ChatTalkingCitizen.java
+++ src/main/java/fr/yro/llmcraft/Citizens/ChatTalkingCitizen.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 public class ChatTalkingCitizen extends TalkingCitizen{
 
     public ChatTalkingCitizen(TalkingCitizenParameters parameters) {
-        this.parameters = parameters;
+        super(parameters);
     }
 
     @Override
@@ -15,24 +15,7 @@ public class ChatTalkingCitizen extends TalkingCitizen{
     }
 
     public void chatChat(String s, CommandSender commandSender){
-        switch(this.getParameters().type){
-            case PERSONAL:
-
-                String identifier = this.getIdentifier(commandSender);
-                String name = commandSender.getName();
-
-                if(!models.containsKey(name)){
-                    IGModel newConversationModel = new IGModel(this.getParameters().modelType,
-                            identifier, this.getParameters().systemAppend);
-                    models.put(name,newConversationModel);
-                }
-                models.get(name).chat(s, commandSender,this.getParameters().range);
-                break;
-
-            case SHARED:
-                models.get("").chat(s, commandSender, this.getParameters().range);
-                break;
-        }
+        this.getModel(commandSender).chat(s, commandSender);
     }
 
 }
